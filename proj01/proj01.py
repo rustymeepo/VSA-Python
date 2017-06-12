b023366d8f17e035ce6a7467a04a0a6c2f230cab
Peabody <peabody@dhcp-129-59-214-53.n1.vanderbilt.edu>
2017-06-12 11:41:05 -0500

    proj1

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -7,4 +7,18 @@
 
 # If you complete extensions, describe your extensions here!
 
-
+# this will work, no problems here.
+user_input = raw_input("Enter in your name. " )
+print 'Hello there,'
+print user_input
+print 'Could you tell me your age'
+user_input = raw_input('please? ')
+print user_input
+yearX = 2017 + 100
+yearY = int(yearX) - int(user_input)
+bday = raw_input("Have you had a birthday yet? ")
+if bday == 'yes':
+    print 'OK! ' + 'You will be 100 in the year ' + str(yearY)
+    else:
+        newYY = yearY - 1
+        print 'well then, happy belated birthday! you will be 100 in the year' + str(newYY)
