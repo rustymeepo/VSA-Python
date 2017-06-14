acdaabf723f54fb5632be140ec8f04bb67e75428
Peabody <peabody@dhcp-129-59-214-53.n1.vanderbilt.edu>
2017-06-14 11:33:45 -0500

    proj06

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -1,5 +1,7 @@
-# Name:
 # Date:
+
+def intersection(lst1, lst2):
+    return list(set(lst1).intersection(lst2))
 
 
 # proj06: Hangman
@@ -12,10 +14,11 @@ import string
 
 WORDLIST_FILENAME = "words.txt"
 
+
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -29,23 +32,48 @@ def load_words():
     print "  ", len(wordlist), "words loaded."
     return wordlist
 
+
 def choose_word(wordlist):
     """
     wordlist (list): list of words (strings)
-
     Returns a word from wordlist at random
     """
     return random.choice(wordlist)
 
+
 # end of helper code
 # -----------------------------------
-count = 0
-# actually load the dictionary of words and point to it with 
+
+# actually load the dictionary of words and point to it with
 # the wordlist variable so that it can be accessed from anywhere
 # in the program
 wordlist = load_words()
+derp = choose_word(wordlist)
+word = list(derp)
+ans_lst = []
+print word
+# your code begins here!
+print "Welcome to the game, Hangman!"
+for x in range(0, len(word)):
+    ans_lst.append('_ ')
+print str(ans_lst)
+print "I am thinking of a word that is " + str(len(word)) + " letters long."
+guess = raw_input()
+guesses = len(word) + 2
+while guesses > 0:
+    print str(ans_lst)
+    if guess == derp:
+        print 'congratulations! you suck!'
+        break
+    ans_lst = []
+    guess = guess[:1]
+    guess = guess + ' '
+    comm_lst = intersection(guess, word)
+    print str(ans_lst)
+    if len(comm_lst) > 0:
+        print 'LEEDLE'
 
-# your code begins here!?.,mnbvcxzzxcvbnm,./
-print choose_word(wordlist)
-while count = 0
-
+    else:
+        guesses -= 1
+        print "Wrong! you have " + str(guesses) + ' guesses left!'
+    guess = raw_input("guess again")
